86f4421e489e5c37cdbb19ef06245093ea37b8a8
unknown <toropov.nikita2004@gmail.com>
2023-05-15 16:30:11 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <div className="landing-wrapper">
+        <Header/>
+        <h1>We are creatives</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import "../css/Header.css"
+
+const Header = () => {
+  return (
+    <header className="d-flex px-5 py-4 justify-content-between align-items-center header">
+        <img src="../../images/logo.svg" alt="Sunnyside logo." />
+        <nav className="main-nav">
+            <ul className="d-flex justify-content-between align-items-center ps-0 mb-0">
+                <li className="list-group-item"><a className="text-decoration-none link" href="#">About</a></li>
+                <li className="list-group-item"><a className="text-decoration-none link" href="#">Services</a></li>
+                <li className="list-group-item"><a className="text-decoration-none link" href="#">Projects</a></li>
+                <li className="list-group-item"><button className="contact-button">CONTACT</button></li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
