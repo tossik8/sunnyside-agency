a79b865d61761057f42d9e6cc1359607ecc10061
unknown <toropov.nikita2004@gmail.com>
2023-05-16 12:08:49 +0200

    feat: create a gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,7 +1,24 @@
-
+import "../css/Gallery.css"
 const Gallery = () => {
   return (
-    <div>Gallery</div>
+    <section>
+      <picture>
+        <source srcSet="../../images/mobile/image-gallery-milkbottles.jpg" media="(max-width: 375px)"/>
+        <img className="gallery-image" src="../../images/desktop/image-gallery-milkbottles.jpg" alt="Milk bottles." />
+      </picture>
+      <picture>
+        <source srcSet="../../images/mobile/image-gallery-orange.jpg" media="(max-width: 375px)"/>
+        <img className="gallery-image" src="../../images/desktop/image-gallery-orange.jpg" alt="An orange." />
+      </picture>
+      <picture>
+        <source srcSet="../../images/mobile/image-gallery-cone.jpg" media="(max-width: 375px)"/>
+        <img className="gallery-image" src="../../images/desktop/image-gallery-cone.jpg" alt="An ice cream cone." />
+      </picture>
+      <picture>
+        <source srcSet="../../images/mobile/image-gallery-sugar-cubes.jpg" media="(max-width: 375px)"/>
+        <img className="gallery-image" src="../../images/desktop/image-gallery-sugarcubes.jpg" alt="Sugar cubes." />
+      </picture>
+    </section>
   )
 }
 
