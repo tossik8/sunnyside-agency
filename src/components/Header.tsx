2e478f61b6f2fbc61d0dbf083624292096187a0a
unknown <toropov.nikita2004@gmail.com>
2023-05-18 20:51:35 +0200

    feat: open a menu on button click

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "../css/Header.css"
 
 const Header = () => {
   const navTag = useRef<HTMLElement>(null);
   const menuButton = useRef<HTMLButtonElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   const setNavigation = () => {
     if(document.documentElement.clientWidth < 710){
@@ -23,8 +24,23 @@ const Header = () => {
   window.onload = () => setNavigation();
   window.onresize = () => setNavigation();
 
+  const handleClick = () => {
+    if(!isOpen){
+      document.getElementsByClassName("menu")[0].classList.add("active");
+      document.getElementsByClassName("menu")[0].classList.remove("inactive");
+      menuButton.current!.style.opacity = "0.4";
+      setIsOpen(true);
+    }
+    else{
+      document.getElementsByClassName("menu")[0].classList.remove("active");
+      document.getElementsByClassName("menu")[0].classList.add("inactive");
+      menuButton.current!.style.opacity = "1";
+      setIsOpen(false);
+    }
+  }
+
   return (
-    <header className="d-flex px-5 py-4 justify-content-between align-items-center header">
+    <header className="d-flex flex-wrap px-5 py-4 justify-content-between align-items-center header">
         <img src="../../images/logo.svg" alt="Sunnyside logo." />
         <nav ref={navTag} className="main-nav inactive">
             <ul className="d-flex justify-content-between align-items-center ps-0 mb-0">
@@ -34,7 +50,15 @@ const Header = () => {
                 <li className="list-group-item"><button className="contact-button">CONTACT</button></li>
             </ul>
         </nav>
-        <button ref={menuButton} className="menu-button px-0 inactive"><img src="../../images/icon-hamburger.svg" alt="Menu icon."/></button>
+        <button onClick={handleClick} ref={menuButton} className="menu-button px-0 inactive"><img src="../../images/icon-hamburger.svg" alt="Menu icon."/></button>
+        <div className="menu mt-3 py-4 inactive">
+          <ul className="d-flex flex-column justify-content-between align-items-center ps-0 mb-0">
+            <li className="list-group-item mb-3"><a className="text-decoration-none link menu-link" href="#about">About</a></li>
+            <li className="list-group-item mb-3"><a className="text-decoration-none link menu-link" href="#services">Services</a></li>
+            <li className="list-group-item mb-3"><a className="text-decoration-none link menu-link" href="#projects">Projects</a></li>
+            <li className="list-group-item"><button className="contact-button contact-menu-button">CONTACT</button></li>
+          </ul>
+        </div>
     </header>
   )
 }
