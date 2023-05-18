7015c3c7c3f85f5ebce0c750991b200c068994dc
unknown <toropov.nikita2004@gmail.com>
2023-05-18 19:44:34 +0200

    feat: change the header layout depending on the width

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,32 @@
+import { useRef } from "react";
 import "../css/Header.css"
 
 const Header = () => {
+  const navTag = useRef<HTMLElement>(null);
+  const menuButton = useRef<HTMLButtonElement>(null);
+
+  const setNavigation = () => {
+    if(document.documentElement.clientWidth < 710){
+      navTag.current!.classList.remove("active");
+      navTag.current!.classList.add("inactive");
+      menuButton.current!.classList.remove("inactive");
+      menuButton.current!.classList.add("active");
+    }
+    else{
+      navTag.current!.classList.remove("inactive");
+      navTag.current!.classList.add("active");
+      menuButton.current!.classList.remove("active");
+      menuButton.current!.classList.add("inactive");
+    }
+  }
+
+  window.onload = () => setNavigation();
+  window.onresize = () => setNavigation();
+
   return (
     <header className="d-flex px-5 py-4 justify-content-between align-items-center header">
         <img src="../../images/logo.svg" alt="Sunnyside logo." />
-        <nav className="main-nav">
+        <nav ref={navTag} className="main-nav inactive">
             <ul className="d-flex justify-content-between align-items-center ps-0 mb-0">
                 <li className="list-group-item"><a className="text-decoration-none link" href="#about">About</a></li>
                 <li className="list-group-item"><a className="text-decoration-none link" href="#services">Services</a></li>
@@ -12,6 +34,7 @@ const Header = () => {
                 <li className="list-group-item"><button className="contact-button">CONTACT</button></li>
             </ul>
         </nav>
+        <button ref={menuButton} className="menu-button px-0 inactive"><img src="../../images/icon-hamburger.svg" alt="Menu icon."/></button>
     </header>
   )
 }
