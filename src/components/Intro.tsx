3b91775e41080d61b2b5bbf71f33369014243f26
unknown <toropov.nikita2004@gmail.com>
2023-05-18 20:02:54 +0200

    fix: add text-center classes

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,7 +5,7 @@ const Intro = () => {
   return (
     <div className="landing-wrapper d-flex flex-column align-items-center">
         <Header/>
-        <h1 className="title">WE ARE CREATIVES</h1>
+        <h1 className="title text-center">WE ARE CREATIVES</h1>
         <img className="arrow" src="../images/icon-arrow-down.svg" alt="Arrow down." />
     </div>
   )
